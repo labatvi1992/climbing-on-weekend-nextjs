b50b70533f83a868daf64e7beb95a05fdaf117aa
michael <danglongvipro@gmail.com>
2021-12-11 11:39:18 +0700

    fix_css

---
+++ pages/category/[slug]/index.js
@@ -0,0 +1,61 @@
+import Articles from "../../components/articles";
+import Breadcrumbs from "../../components/breadcrumbs";
+import { fetchAPI } from "../../lib/api";
+import Seo from "../../components/seo";
+
+const Category = ({ category, categories }) => {
+  const seo = {
+    metaTitle: category.name,
+    metaDescription: `All ${category.name} articles`,
+  };
+
+  return (
+    <main id="main">
+      <Seo seo={seo} />
+      <Breadcrumbs
+        title={category.name}
+        items={[
+          { name: "Trang chủ", slug: "/" },
+          { name: "Bài viết", slug: "/category" },
+        ]}
+      />
+      <Articles articles={category.articles} categories={categories} />
+    </main>
+  );
+};
+
+export async function getStaticPaths() {
+  let categories = [];
+  try {
+    categories = await fetchAPI("/categories");
+  } catch (error) {
+    console.log(error);
+  }
+  return {
+    paths: categories.map((category) => ({
+      params: {
+        slug: category.slug,
+      },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  try {
+    const category = (await fetchAPI(`/categories?slug=${params.slug}`))[0];
+    const categories = await fetchAPI("/categories");
+    return {
+      props: { category, categories },
+      revalidate: 1,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      props: { category: {}, categories: [] },
+      revalidate: 1,
+    };
+  }
+}
+
+export default Category;
